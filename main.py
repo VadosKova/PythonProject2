7a899fc2affdcdd14442c81326e673704446d7d9
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-02-26 10:28:33 +0200

    Updated main file, added choice

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello world")
+from length_converter import LengthConverter
+from mass_converter import MassConverter
+from time_converter import TimeConverter
+from temp_converter import TemperatureConverter
+from currency_converter import CurrencyConverter
+
+while True:
+    print("Choose conversion type:")
+    print("1. Length")
+    print("2. Mass")
+    print("3. Time")
+    print("4. Temperature")
+    print("5. Currency")
+    print("6. Exit")
+
+    type = input("Enter choice: ")
+
